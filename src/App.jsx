5e838f917ec11aae95fae873e37c0ca01e297292
davad <tapetonjab@gmail.com>
2025-01-29 23:37:14 +0100

    main_pert_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,64 @@
 /*
-Chap 7 Le hook useEffect 
-v1
+Chap 7 Le hook useEffect partie A.b
+v2
 */
 import { useEffect, useState } from 'react'
 import { Input } from './components/froms/Input.jsx'
-import { Checkbox } from './components/froms/Checkbox.jsx'
+// import { Checkbox } from './components/froms/Checkbox.jsx'
 
 function App() {
-  const [showInput, setShowInput] = useState(true)
+  const [duration, setDuration] = useState(5)
+  const [secondsLeft, setSecondsLeft] = useState(duration)
 
-  return <div className='container my-3 stack'>
-    <Checkbox
-        checked={showInput}
-        onChange={setShowInput}
-        id="titleshow"
-        label="Afficher le champs titre"
+  const handleChange = (v) => {
+    setDuration(v)
+    setSecondsLeft(v)
+  }
+
+// console.log('render')
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSecondsLeft(v => {
+        if (v <= 1) {
+          clearInterval(timer)
+          return 0
+        }
+        return v - 1
+      })
+    }, 1000)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [duration]);
+
+/*
+  return <div className='vstack gap-2'>
+    <Input
+      value={duration}
+      onChange={handleChange}
+      placeholder="Timer..."
     />
-    {showInput && <EditTitle />}
-    <div style={{height: '300vh'}}></div>
+    <p>
+        Décompte : {secondsLeft}
+    </p>
   </div>
 }
-
-function EditTitle () {
-  const [title, setTitle] = useState('')
-  const [firstname, setFirstname] = useState('')
-  const [y, setY] = useState(0)
-
-
-  useEffect(() => {
-    const originalTitle = document.title
-    return () => {
-      document.title = originalTitle
-    }
-  }, []);
-
-  useEffect(() => {
-    document.title = title
-  }, [title]);
-
-  /*
-  useEffect(() => {
-    const handler = (e) => {
-      console.log('scroll')
-      setY(window.scrollY)
-    }
-    window.addEventListener('scroll', handler)
-    return () => {
-      window.removeEventListener('scroll', handler)
-    }
-}, []);
 */
 
-  return <div className='vstack gap-2'>
-    <div>
-      Scroll : {y}
+  return (
+    <div className="vstack gap-2 text-center p-3 bg-light rounded">
+      <Input
+        value={duration}
+        onChange={handleChange}
+        placeholder="Timer..."
+        className="form-control text-center"
+      />
+      <p className="fw-bold text-primary">
+        Décompte : {secondsLeft}
+      </p>
     </div>
-    <Input
-      placeholder="Modifier le titre"
-      value={title}
-      onChange={setTitle}
-    />   
-    <Input
-         placeholder="Prénom"
-         value={firstname}
-         onChange={setFirstname}
-    />
-  </div>
+  );
 }
 
 export default App
