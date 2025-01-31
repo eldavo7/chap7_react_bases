3a10d6096fc38e18905fd368d7e8b34327deaf46
davad <tapetonjab@gmail.com>
2025-01-31 18:13:38 +0100

    part_3.1

--- README.md
+++ README.md
@@ -1,18 +1,27 @@
 # React + Vite
-#### Dossier en cours :
-- /Users/.../../../2025/reactjs/cours_react/chap7_react_bases
 
-#### Dossier modifier et / ou ajouter depuis origin/main 
-- index.html
-    - src/App.jsx
-    - src/components/froms/Input.jsx
+> [!NOTE]
+> Dossier en cours :
+> /Users/.../../../2025/reactjs/cours_react/chap7_react_bases
+> Dossier modifier et / ou ajouter depuis origin/main
 
-
-    - src/components/froms/Checkbox.jsx  ->  Partie 1 : le hook useEffect
-    - src/components/froms/Input.jsx     ->  Partie 1 : le hook useEffect
- 
-
-- Partie 1 : le hook useEffect  ->   branch : main & main_part_2 
+- Partie 1 : le hook useEffect     ->     branch : main & main_part_2 
     * Cela represente 2 chapitres donc 2 branch
-- Partie 2 : le hook useMemo, (useId)  ->   branch : part_2
+    * fichiers modifier et/ou creer :
+        * src/App.jsx
+        * src/components/froms/Checkbox.jsx
+        * src/components/froms/Input.jsx
+
+- Partie 2 : le hook useMemo, (useId)      ->     branch : part_2
+    * fichiers modifier et/ou creer :
+        * src/App.jsx
+        * src/components/froms/Input.jsx
+
+- Partie 3 : le hook useRef      ->      branch : part_3.1
+    * fichiers modifier et/ou creer :
+        * src/App.jsx
+- Partie 3 : le hook useRef      ->      branch : part_3.2
+    * fichiers modifier et/ou creer :
+        * src/App.jsx
+        * src/components/froms/Input.jsx
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,28 @@
-/*   - Partie 2 : le hook useMemo, (useId)  ->   branch : part_2     */
+/*   - Partie 3 : le hook useRef  ->   branch : part_3.1     */
 
-import { useId, useMemo, useState } from 'react'
-import { Input } from './components/froms/Input.jsx'
-// import { Checkbox } from './components/froms/Checkbox.jsx'
+import { useEffect, useId, useMemo, useRef, useState } from 'react';
+import { Input } from './components/froms/Input.jsx';
+// import { Checkbox } from './components/froms/Checkbox.jsx';
 
 function App() {
-  const [firstname, setFirstname] = useState('John')
-  const [password, setPassword] = useState('MotDePasse')
-  const security = passwordSecurity(password)
-  const random = useMemo(() => Math.random(), [])
 
-return <div className="container my-3 vstack gap-2">
-    <Input 
-      label="Nom d'utilisateur"
-      value={firstname}
-      onChange={setFirstname} 
-    />
-    <div>
-      <Input
-        label="Password" 
-        type="password" 
-        value={password}
-        onChange={setPassword} 
-      />
-      Sécurité : {security}
-      </div>
-    </div>
-  }
+  const prefixRef = useRef()
+  const [prefix, setPrefix] = useState('')
+  prefixRef.current = prefix
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      console.log(prefixRef.current)
+    }, 1000)
+    return () => {
+      clearInterval(timer)
+    }
+  }, []);
 
-function passwordSecurity (password) {
-  // Fausse lenteur
-  let startTime = performance.now()
-  while (performance.now() - startTime < 200 ) {
-  }
-
-  if (password.length < 3) {
-    return 'Faible'
-  } else if (password.length < 6) {
-    return 'Moyen'
-  }
-  return 'Fort';
-
+  return <div>
+    <Input label="prefix" value={prefix} onChange={setPrefix}/>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, labore? Id temporibus laboriosam reprehenderit dicta, vitae doloribus facere doloremque? Deleniti totam eaque voluptas illum consectetur ratione enim iure perspiciatis accusamus.
+  </div>
 }
   
 
